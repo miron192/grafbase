31cf473755dd8e14f0353d89f3f3b01c6d9bbd19
Miron Catalin <catalinmiron093@gmail.com>
2023-08-06 18:50:03 +0300

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,75 @@
 import Image from "next/image";
+import { fetchAllProjects } from "@/lib/actions";
+import { ProjectInterface } from "@/common.types";
+import ProjectCard from "@/components/ProjectCard";
+import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 
-export default function Home() {
+type ProjectsSearch = {
+  projectSearch: {
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+  };
+};
+
+type SearchParams = {
+  category?: string | undefined;
+  endcursor?: string;
+};
+
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
+
+type Props = { searchParams: SearchParams };
+
+export default async function Home({
+  searchParams: { category, endcursor },
+}: Props) {
+  const data = (await fetchAllProjects(category, endcursor)) as ProjectsSearch;
+
+  const projectsToDisplay = data?.projectSearch?.edges || [];
+
+  if (projectsToDisplay.length === 0) {
+    return (
+      <section className="flexStart flex-col paddings">
+        <Categories />
+        <p className="no-restult-text text-center">
+          No projects found, go create some first
+        </p>
+      </section>
+    );
+  }
+
+  const pagination = data.projectSearch?.pageInfo;
+
   return (
-    <section className="flex-start flex-col paddings mb-16">
-      <h1>Categories</h1>
-      <h1>Posts</h1>
-      <h1>LoadMore</h1>
+    <section className="flexStart flex-col paddings mb-16">
+      <Categories />
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={`${node?.id}`}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy.name}
+            avatarUrl={node?.createdBy.avatarUrl}
+            userId={node?.createdBy.id}
+          />
+        ))}
+      </section>
+      <LoadMore
+        startCursor={pagination?.startCursor}
+        endCursor={pagination?.endCursor}
+        hasPreviousPage={pagination.hasPreviousPage}
+        hasNextPage={pagination.hasNextPage}
+      />
     </section>
   );
 }
--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,4 +1,48 @@
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+import Button from "./Button";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
+
 const AuthProvider = () => {
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      console.log(res);
+
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <Button
+            key={i}
+            handleClick={() => signIn(provider?.id)}
+            title="Sign In"
+          />
+        ))}
+      </div>
+    );
+  }
+
   return <div>AuthProvider</div>;
 };
 
